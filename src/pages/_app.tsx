ee2171d447400972a54c53767eadd25e68c3acef
Nicolas Kasevich <n.kasevich@gmail.com>
2023-05-14 03:59:00 +0200

    Mui Material installed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import { useState } from "react";
 import { QueryClient, Hydrate, QueryClientProvider } from "react-query";
 

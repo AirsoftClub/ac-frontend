5c3a0860c821a61eccf6dc356daa04c293c9742c
Nicolas Kasevich <nicolas_kasevich@epam.com>
2023-08-04 17:42:54 +0200

    added no-referrer meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <meta name="referrer" content="no-referrer" />
       <Head />
       <body>
         <Main />
